d211842abcffda40e51d6d4161adce262c3861d8
Miquel Gatius <154758428+MiquelGatiusFernandez@users.noreply.github.com>
2024-01-07 19:00:07 +0100

    revised rock paper scissors

--- Rock-paper-scissors/index.js
+++ Rock-paper-scissors/index.js
@@ -1,37 +1,44 @@
 let options = ["rock", "paper", "scissors"];
 let lives = 3;
-
+let rounds = 0;
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * options.length);
     return options[randomNumber];
 }
 
-function game() {
-    for (let i = 0; i < 5 && lives > 0; i++) {
+function game(playerSelection) {
+    if (lives > 0 && rounds < 5) {
         let computerSelection = getComputerChoice();
-        let playerSelection = prompt("Choose rock, paper, scissors").toLocaleLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("You have " + lives + " lives left.");
-    }
-    if (lives == 0) {
-        console.log("You have lost the game.");
-    }else{
-        console.log("You have won the game.");
+        document.querySelector('.result').textContent = playRound(playerSelection, computerSelection);
+        document.querySelector('.lives').textContent = `You have ${lives} lives left.`;
+        rounds++;
+        if (lives <= 0) {
+            alert("You've lost.");
+        } else if (rounds >= 5) {
+            alert("You've won.");
+        }
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("It's a tie!\nPlayer chose: " + playerSelection + "\ncomputer chose: " + computerSelection);
-        return playRound(playerSelection, getComputerChoice());
+        return "It's a tie!\nPlayer chose: " + playerSelection + "\ncomputer chose: " + computerSelection + " \n";
     } else if ((playerSelection === options[0] && computerSelection === options[2]) || (playerSelection === options[1] && computerSelection === options[0]) || (playerSelection === options[2] && computerSelection === options[1])) {
-        return result = "It's a win!\nPlayer chose: " + playerSelection + "\ncomputer chose: " + computerSelection;
+        return "It's a win!\nPlayer chose: " + playerSelection + "\ncomputer chose: " + computerSelection;
     } else {
         lives--;
-        return result = "It's a lose!\nPlayer chose: " + playerSelection + "\ncomputer chose: " + computerSelection;
+        return "It's a lose!\nPlayer chose: " + playerSelection + "\ncomputer chose: " + computerSelection;
     }
 }
 
-game();
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+            game(event.target.value);
+    });
+});
+
+
 
 
